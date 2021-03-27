07bdcbad4df8a4728e891907295cc4607ea04261
Barkin Ozer <c.barkinozer@gmail.com>
2021-03-27 22:06:04 +0300

    Listing  operations are done for category,product,customer. Sale and Help pages still requires works.

---
+++ StockMVC/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StockMVC.Controllers;
+using StockMVC.Models.Entity;
+
+namespace StockMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        MvcStockDBEntities db = new MvcStockDBEntities();
+        public ActionResult Index()
+        {
+            var customers = db.TblCustomers.ToList();
+            return View(customers);
+        }
+    }
+}
