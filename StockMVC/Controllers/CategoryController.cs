440650b31f85f36af703a982223410d89436464c
Barkin Ozer <c.barkinozer@gmail.com>
2021-03-27 21:09:28 +0300

    Categories listed from DB on index page

--- StockMVC/Controllers/CategoryController.cs
+++ StockMVC/Controllers/CategoryController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StockMVC.Models.Entity;
 
 namespace StockMVC.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        MvcStockDBEntities db = new MvcStockDBEntities();
         public ActionResult Index()
         {
-            return View();
+            var categories = db.TblCategories.ToList();
+            return View(categories);
         }
     }
 }
