6535a0f93442db8158b82ca588716d70df304dd4
Barkin Ozer <c.barkinozer@gmail.com>
2021-03-27 21:25:29 +0300

    Product listing from db is done

---
+++ StockMVC/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StockMVC.Models.Entity;
+
+namespace StockMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        MvcStockDBEntities db = new MvcStockDBEntities();
+        public ActionResult Index()
+        {
+            var products = db.TblProducts.ToList();
+            return View(products);
+        }
+    }
+}
