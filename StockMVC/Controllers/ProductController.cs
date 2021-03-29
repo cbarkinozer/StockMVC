d898be1e0e6587fd02ab66b9692fbe4a0eebe515
Barkin Ozer <c.barkinozer@gmail.com>
2021-03-30 02:01:10 +0300

    AddProduct added In AddProduct, dropdownlist of categories added from database

--- StockMVC/Controllers/ProductController.cs
+++ StockMVC/Controllers/ProductController.cs
@@ -16,5 +16,23 @@ namespace StockMVC.Controllers
             var products = db.TblProducts.ToList();
             return View(products);
         }
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            List<SelectListItem> selectListItems = (from i in db.TblCategories.ToList()
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = i.CategoryName,
+                                                        Value = i.CategoryID.ToString()
+                                                    }).ToList();
+            ViewBag.itemList = selectListItems;
+            return View();
+        }
+        public ActionResult AddProduct(TblProducts tblProducts)
+        {
+            db.TblProducts.Add(tblProducts);
+            db.SaveChanges();
+            return View();
+        }
     }
 }
